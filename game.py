674d13eebcd5af15babd9c61df2fbac1bb7eb4db
Shane-Crawford-tech <165920119+Shane-Crawford-tech@users.noreply.github.com>
2024-04-25 12:29:45 -0500

    Create game.py

---
+++ game.py
@@ -0,0 +1,21 @@
+#import _.py
+import random
+import Button
+import SOT
+
+class startGame():
+    def __init__():
+        pass
+
+class easy(startGame):
+    def __init__():
+        pass
+
+class normal(startGame):
+    def __init__():
+        pass
+
+class hard(startGame):
+    def __init__():
+        pass
+        
