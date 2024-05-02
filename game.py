e2cab7495c216ae2e1ed2eefb6675db32a717b36
Shane-Crawford-tech <165920119+Shane-Crawford-tech@users.noreply.github.com>
2024-05-02 00:13:43 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,21 +1,10 @@
 #import _.py
 import random
-import Button
+import pygame
+import sys
+import pygame_menu as pm 
+from Button import Button
 import SOT
+from initializing import easy
 
-class startGame():
-    def __init__():
-        pass
-
-class easy(startGame):
-    def __init__():
-        pass
-
-class normal(startGame):
-    def __init__():
-        pass
-
-class hard(startGame):
-    def __init__():
-        pass
-        
+SOT.main_menu()
